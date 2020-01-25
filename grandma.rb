4af31b4fb2234fa60f6e11c17b14b0027b8c3bea
james Johnson <2013matthew2013@gmail.com>
2020-01-25 19:08:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,8 @@
 def speak_to_grandma
   IF phase == "I LOVE YOU TO GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-  elsif phase.UPCASE
-
-  "HUH?! SPEAK UP SONNY!"
+  elsif phase.DOWNCASE
+"HUH?! SPEAK UP SONNY!"
 else "NO, NOT SINCE 1938!"
 
 end
